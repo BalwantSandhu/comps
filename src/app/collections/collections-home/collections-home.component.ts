f2523f2a6d608323ed86eb8f12c46a762ba2a21a
BalwantSandhu <bsingh3@unb.ca>
2026-01-02 00:22:19 -0400

    Updated collection home component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { TableComponent } from "../table/table.component";
 import { DividerComponent } from "../../shared/divider/divider.component";
 import { NgClass } from "@angular/common";
+import { BiographyComponent } from "../biography/biography.component";
+import { ModsRoutingModule } from "../../mods/mods-routing.module";
+import { TabsComponent } from "../tabs/tabs.component";
+
 
 @Component({
   selector: 'app-collections-home',
-  imports: [TableComponent, DividerComponent, NgClass],
+  imports: [TableComponent, DividerComponent, NgClass, BiographyComponent, ModsRoutingModule, TabsComponent],
+  standalone: true,
   templateUrl: './collections-home.component.html',
   styleUrl: './collections-home.component.css'
 })
