169a31fb1a8ffd7b85d0940a68dfe18725755d8e
BalwantSandhu <bsingh3@unb.ca>
2026-01-02 00:23:08 -0400

    Added tabs component

---
+++ src/app/collections/tabs/tabs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ModsRoutingModule } from "../../mods/mods-routing.module";
+
+
+@Component({
+  selector: 'app-tabs',
+  imports: [ModsRoutingModule],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.css'
+})
+export class TabsComponent {
+
+}
