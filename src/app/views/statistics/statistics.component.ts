fdcfc24a0e09bb2dc93080fe8558520541534556
BalwantSandhu <bsingh3@unb.ca>
2026-01-02 13:53:41 -0400

    Finished up on views

--- src/app/views/statistics/statistics.component.ts
+++ src/app/views/statistics/statistics.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-statistics',
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.css'
 })
 export class StatisticsComponent {
-
+  @Input() data: any = [];
 }
